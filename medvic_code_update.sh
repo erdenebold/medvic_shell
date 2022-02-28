7750648f222865abc78beb8e4d66c35c8f984a11
erka <erdenebold10@gmail.com>
2022-02-28 11:36:57 +0800

    add code update

---
+++ medvic_code_update.sh
@@ -0,0 +1,8 @@
+cd /opt/odoo/medvic_erp
+sudo git pull
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server start
